dffb3c960aedfab7abb52d082f410c2db8ebf078
FLORBELA FREITAS OLIVEIRA <21276327@CFP127ALUNO.LOCAL>
2021-10-01 16:54:51 -0300

    código - 01/10

--- src/br/senai/sp/jandira/model/Conta.java
+++ src/br/senai/sp/jandira/model/Conta.java
@@ -1,50 +1,69 @@
 package br.senai.sp.jandira.model;
 
+import br.senai.sp.jandira.lista.TipoConta;
+
 public class Conta {
-	
-	public String tipo;
+
+	private TipoConta tipo;
 	private String numero;
-	public String numeroAgencia;
-	public String titular;
+	private String numeroAgencia;
+	public Cliente titular;
 	private double saldo;
-	
+
+	// Método construtor
 	public Conta(String numeroConta) {
 		numero = numeroConta;
 	}
+
+	public void setTipo(TipoConta tipo) {
+		this.tipo = tipo;
+	}
 	
+	public TipoConta getTipo() {
+		return tipo;
+	}
+	
+	public void setNumeroAgencia(String numeroAgencia) {
+		this.numeroAgencia = numeroAgencia;
+	}
+	
+	public String getNumeroAgencia() {
+		return numeroAgencia;
+	}
+
 	public void depositar(double valorDeposito) {
-		if(valorDeposito <= 0) {
+		if (valorDeposito <= 0) {
 			System.out.println("Valor inválido!");
 		} else {
 			saldo += valorDeposito;
 		}
-		
+
 	}
-	
+
 	public boolean sacar(double valorSaque) {
-		if(valorSaque <= 0) {
+		if (valorSaque <= 0) {
 			System.out.println("Valor inválido!");
 			return false;
-		} else if(valorSaque > saldo) {
+		} else if (valorSaque > saldo) {
 			System.out.println("Saldo insuficiente!");
 			return false;
 		} else {
 			saldo -= valorSaque;
 			return true;
 		}
-		
+
 	}
-	
+
 	public void transferir(Conta contaDestino, double valorTransferencia) {
 		boolean resultado = sacar(valorTransferencia);
-		if(resultado) {
+		if (resultado) {
 			contaDestino.depositar(valorTransferencia);
 		} else {
 			System.out.println("Não foi possível realizar a transferência!");
 		}
-		
+
 	}
-	
+
 	public void exibirDetalhes() {
 		System.out.println();
 		System.out.println("---------------------");
@@ -54,5 +73,5 @@ public class Conta {
 		System.out.printf("Tipo: %s\n", tipo);
 		System.out.printf("Saldo: %s\n", saldo);
 	}
-	
+
 }
